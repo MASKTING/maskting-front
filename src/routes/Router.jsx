8c92ecf9ebe0d4b7ed5445e68ad16564bea5bacc
Haena <kimhn200605@gmail.com>
2022-08-16 13:08:01 +0900

    feat(routes): add BasicInfo route

--- src/routes/Router.jsx
+++ src/routes/Router.jsx
@@ -1,11 +1,11 @@
 import { BrowserRouter as Routers, Routes, Route } from 'react-router-dom';
-import ExamplePage from '../pages/ExamplePage';
+import BasicInfoPage from '../pages/BasicInfo';
 
 function Router() {
 	return (
 		<Routers>
 			<Routes>
-				<Route path="/" element={<ExamplePage />} />
+				<Route path="/" element={<BasicInfoPage />} />
 			</Routes>
 		</Routers>
 	);
