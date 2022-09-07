34a45eb113754fc7bfe853db2c768c15a1c5466d
Haena <kimhn200605@gmail.com>
2022-09-07 09:51:31 +0900

    feat(router): add moreInfo route

--- src/routes/Router.jsx
+++ src/routes/Router.jsx
@@ -1,11 +1,15 @@
 import { BrowserRouter as Routers, Routes, Route } from 'react-router-dom';
 import BasicInfoPage from '../pages/BasicInfo';
+import LocationPage from '../pages/LocationPage';
+import MoreInfoPage from '../pages/MoreInfo';
 
 function Router() {
 	return (
 		<Routers>
 			<Routes>
-				<Route path="/" element={<BasicInfoPage />} />
+				<Route exact path="/" element={<BasicInfoPage />} />
+				<Route path="/location" element={<LocationPage />} />
+				<Route path="/moreInfo" element={<MoreInfoPage />} />
 			</Routes>
 		</Routers>
 	);
