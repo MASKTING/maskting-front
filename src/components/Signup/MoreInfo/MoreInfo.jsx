f9a248516d06faa30482af3b01945f749b45440f
Haena <kimhn200605@gmail.com>
2022-10-01 23:55:08 +0900

    feat: moreInfo > 라디오버튼 커스텀 적용

--- src/components/Signup/MoreInfo/MoreInfo.jsx
+++ src/components/Signup/MoreInfo/MoreInfo.jsx
@@ -9,14 +9,15 @@ function MoreInfo() {
 	const { register, handleSubmit, formState } = useForm();
 	const navigate = useNavigate();
 	const [submit, setSubmit] = useState(false);
-	const [value, setValue] = useState({
-		duty: '',
-		smoking: '',
-		religion: '',
-		height: '',
-		drinking: '0',
-		bodyType: '0',
+	const [radio, setRadio] = useState({
+		duty: null,
+		smoking: null,
+		religion: null,
+		height: null,
+		drinking: null,
+		bodyType: null,
 	});
+
 	const msg = {
 		drinking: {
 			1: '술을 마시지 않아요',
@@ -33,149 +34,184 @@ function MoreInfo() {
 			5: '통통한 편이에요',
 		},
 	};
-	const onValid = data => {
-		if (value.drinking && value.bodyType) {
-			console.log(data);
-		} else {
-			setSubmit(true);
-		}
+
+	const radioChange = e => {
+		e.preventDefault();
+		setRadio({
+			...radio,
+			[e.target.name]: e.target.value,
+		});
 	};
-	const onInvalid = e => {
-		console.log(e);
-		setSubmit(true);
-		// navigate('')
-	};
+
+	const onValid = () => {};
+	const onInvalid = e => {};
+
 	const handlePrevBtn = () => {
 		navigate('/hobby');
 	};
-	const onSelected = e => {
-		const { name, value } = e.target;
-		setValue(prevState => ({
-			...prevState,
-			[name]: value,
-		}));
-	};
-	const onChangeSlide = e => {
-		const { name, value } = e.target;
-		setValue(prevState => ({
-			...prevState,
-			[name]: value,
-		}));
-	};
-	const handleNextBtn = () => {};
 
+	const handleNextBtn = () => {
+		setSubmit(true);
+		if (
+			radio.duty &&
+			radio.smoking &&
+			radio.religion &&
+			radio.drinking &&
+			radio.height &&
+			radio.bodyType
+		) {
+			// navigate('/');
+		}
+	};
 	return (
 		<Wrapper titleMessage="마지막 질문이에요!">
 			<S.NoticeText>매칭 시에만 활용되며, 상대방에게는 정보가 공개되지 않아요</S.NoticeText>
 			<S.Form onSubmit={handleSubmit(onValid, onInvalid)}>
 				<S.Content>
 					<S.BasicInfoWrapper>
-						{formState.errors?.duty ? (
-							<S.ErrorMessage>{formState.errors?.duty?.message}</S.ErrorMessage>
+						{submit && radio.duty === null ? (
+							<S.ErrorMessage>군대 여부를 선택해주세요</S.ErrorMessage>
 						) : (
-							<S.Label htmlFor="duty">군대 여부</S.Label>
+							<S.Label>군대 여부</S.Label>
 						)}
-						<S.NarrowInput
-							type="radio"
-							name="duty"
-							value="true"
-							onClick={onSelected}
-							{...register('duty', {
-								required: {
-									value: true,
-									message: '군대 여부를 선택해주세요',
-								},
-							})}
-						/>
-						<S.NarrowInput
-							type="radio"
-							name="duty"
-							value="false"
-							onClick={onSelected}
-							{...register('duty')}
-						/>
+						<S.NarrowButton focused={radio.duty === '군필'}>
+							<S.RadioLabel htmlFor="군필">
+								군필
+								<S.NarrowInput
+									type="radio"
+									name="duty"
+									id="군필"
+									value="군필"
+									onClick={radioChange}
+									{...register('duty')}
+								/>
+							</S.RadioLabel>
+						</S.NarrowButton>
+						<S.NarrowButton focused={radio.duty === '미필'}>
+							<S.RadioLabel htmlFor="미필">
+								미필
+								<S.NarrowInput
+									type="radio"
+									name="duty"
+									id="미필"
+									value="미필"
+									onClick={radioChange}
+									{...register('duty')}
+								/>
+							</S.RadioLabel>
+						</S.NarrowButton>
 					</S.BasicInfoWrapper>
 					<S.BasicInfoWrapper>
-						{formState.errors?.smoking ? (
-							<S.ErrorMessage>{formState.errors?.smoking?.message}</S.ErrorMessage>
+						{submit && radio.smoking === null ? (
+							<S.ErrorMessage>흡연 여부를 선택해주세요</S.ErrorMessage>
 						) : (
-							<S.Label htmlFor="smoking">흡연 여부</S.Label>
+							<S.Label>흡연 여부</S.Label>
 						)}
-						<S.NarrowInput
-							type="radio"
-							name="smoking"
-							value="true"
-							onClick={onSelected}
-							{...register('smoking', {
-								required: {
-									value: true,
-									message: '흡연 여부를 선택해주세요',
-								},
-							})}
-						/>
-						<S.NarrowInput
-							type="radio"
-							name="smoking"
-							value="false"
-							onClick={onSelected}
-							{...register('smoking')}
-						/>
+						<S.NarrowButton focused={radio.smoking === '흡연'}>
+							<S.RadioLabel htmlFor="흡연">
+								흡연
+								<S.NarrowInput
+									type="radio"
+									name="smoking"
+									id="흡연"
+									value="흡연"
+									onClick={radioChange}
+									{...register('smoking')}
+								/>
+							</S.RadioLabel>
+						</S.NarrowButton>
+						<S.NarrowButton focused={radio.smoking === '비흡연'}>
+							<S.RadioLabel htmlFor="비흡연">
+								비흡연
+								<S.NarrowInput
+									type="radio"
+									name="smoking"
+									id="비흡연"
+									value="비흡연"
+									onClick={radioChange}
+									{...register('smoking')}
+								/>
+							</S.RadioLabel>
+						</S.NarrowButton>
 					</S.BasicInfoWrapper>
 					<S.WideInfoWrapper>
-						{formState.errors?.religion ? (
-							<S.ErrorMessage>{formState.errors?.religion?.message}</S.ErrorMessage>
+						{submit && radio.religion === null ? (
+							<S.ErrorMessage>종교를 선택해주세요</S.ErrorMessage>
 						) : (
-							<S.Label htmlFor="religion">종교</S.Label>
+							<S.LongLabel>종교</S.LongLabel>
 						)}
-						<S.NarrowInput
-							type="radio"
-							name="religion"
-							value="기독교"
-							onClick={onSelected}
-							{...register('religion', {
-								required: {
-									value: true,
-									message: '종교를 선택해주세요',
-								},
-							})}
-						/>
-						<S.NarrowInput
-							type="radio"
-							name="religion"
-							value="천주교"
-							onClick={onSelected}
-							{...register('religion')}
-						/>
-						<S.NarrowInput
-							type="radio"
-							name="religion"
-							value="불교"
-							onClick={onSelected}
-							{...register('religion')}
-						/>
-
-						<S.NarrowInput
-							type="radio"
-							name="religion"
-							value="기타"
-							onClick={onSelected}
-							{...register('religion')}
-						/>
-
-						<S.WideInput
-							type="radio"
-							name="religion"
-							value="무교"
-							onClick={onSelected}
-							{...register('religion')}
-						/>
+						<S.NarrowButton focused={radio.religion === '기독교'}>
+							<S.RadioLabel htmlFor="기독교">
+								기독교
+								<S.NarrowInput
+									type="radio"
+									name="religion"
+									id="기독교"
+									value="기독교"
+									onClick={radioChange}
+									{...register('religion')}
+								/>
+							</S.RadioLabel>
+						</S.NarrowButton>
+						<S.NarrowButton focused={radio.religion === '천주교'}>
+							<S.RadioLabel htmlFor="천주교">
+								천주교
+								<S.NarrowInput
+									type="radio"
+									name="religion"
+									id="천주교"
+									value="천주교"
+									onClick={radioChange}
+									{...register('religion')}
+								/>
+							</S.RadioLabel>
+						</S.NarrowButton>
+						<S.NarrowButton focused={radio.religion === '불교'}>
+							<S.RadioLabel htmlFor="불교">
+								불교
+								<S.NarrowInput
+									type="radio"
+									name="religion"
+									id="불교"
+									value="불교"
+									onClick={radioChange}
+									{...register('religion')}
+								/>
+							</S.RadioLabel>
+						</S.NarrowButton>
+						<S.NarrowButton focused={radio.religion === '기타'}>
+							<S.RadioLabel htmlFor="기타">
+								기타
+								<S.NarrowInput
+									type="radio"
+									name="religion"
+									id="기타"
+									value="기타"
+									onClick={radioChange}
+									{...register('religion')}
+								/>
+							</S.RadioLabel>
+						</S.NarrowButton>
+						<S.WideButton focused={radio.religion === '무교'}>
+							<S.RadioLabel htmlFor="무교">
+								무교
+								<S.NarrowInput
+									type="radio"
+									name="religion"
+									id="무교"
+									value="무교"
+									onClick={radioChange}
+									{...register('religion')}
+								/>
+							</S.RadioLabel>
+						</S.WideButton>
 					</S.WideInfoWrapper>
 					<S.BasicInfoWrapper>
-						{submit && value.drinking === '0' ? (
+						{submit && radio.drinking === null ? (
 							<S.ErrorMessage>음주 빈도를 선택해주세요</S.ErrorMessage>
 						) : (
 							<S.LongLabel htmlFor="drinking">
-								음주 빈도 <S.DegreeMessage>{msg.drinking[value.drinking]}</S.DegreeMessage>
+								음주 빈도 <S.DegreeMessage>{msg.drinking[radio.drinking]}</S.DegreeMessage>
 							</S.LongLabel>
 						)}
 						<S.Slider
@@ -183,10 +219,10 @@ function MoreInfo() {
 							name="drinking"
 							min="1"
 							max="5"
-							// value="1"
+							// radio="1"
 							step="1"
-							degree={(parseInt(value.drinking) - 1) * 25}
-							onChange={onChangeSlide}
+							degree={(parseInt(radio.drinking) - 1) * 25}
+							onChange={radioChange}
 						/>
 					</S.BasicInfoWrapper>
 					<S.BasicInfoWrapper>
@@ -202,14 +238,15 @@ function MoreInfo() {
 									message: '키를 입력해주세요',
 								},
 							})}
+							onChange={radioChange}
 						/>
 					</S.BasicInfoWrapper>
 					<S.BasicInfoWrapper>
-						{submit && value.bodyType === '0' ? (
+						{submit && radio.bodyType === null ? (
 							<S.ErrorMessage>체형을 선택해주세요</S.ErrorMessage>
 						) : (
 							<S.LongLabel htmlFor="bodyType">
-								체형 <S.DegreeMessage>{msg.bodyType[value.bodyType]}</S.DegreeMessage>
+								체형 <S.DegreeMessage>{msg.bodyType[radio.bodyType]}</S.DegreeMessage>
 							</S.LongLabel>
 						)}
 						<S.Slider
@@ -217,16 +254,12 @@ function MoreInfo() {
 							name="bodyType"
 							min="1"
 							max="5"
-							// value="1"
+							// radio="1"
 							step="1"
-							degree={(parseInt(value.bodyType) - 1) * 25}
-							onChange={onChangeSlide}
+							degree={(parseInt(radio.bodyType) - 1) * 25}
+							onChange={radioChange}
 						/>
 					</S.BasicInfoWrapper>
-					<S.WideInfoWrapper />
-					<S.WideInfoWrapper />
-					<S.WideInfoWrapper />
-					<S.WideInfoWrapper />
 				</S.Content>
 				<NavigateButton handlePrevBtn={handlePrevBtn} handleNextBtn={handleNextBtn} />
 			</S.Form>
