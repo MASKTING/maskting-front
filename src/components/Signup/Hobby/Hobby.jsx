be99df137d7ce20e995e021090983513e5eda6e2
규성 <gueit214@naver.com>
2022-10-23 19:23:49 +0900

    test : develop 브랜치 테스트

--- src/components/Signup/Hobby/Hobby.jsx
+++ src/components/Signup/Hobby/Hobby.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'react';
+import { useState } from 'react';
 import { useForm } from 'react-hook-form';
 import { useNavigate } from 'react-router-dom';
 import { NavigateButton } from '../../Button/Button';
