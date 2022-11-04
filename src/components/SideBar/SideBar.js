e55e0bddf6ab6ec443489ad027aa2b38eae742db
guesung <62178788+gueit214@users.noreply.github.com>
2022-11-04 20:17:39 +0900

    feat : SideBar 컴포넌트 생성

---
+++ src/components/SideBar/SideBar.js
@@ -0,0 +1,12 @@
+import * as S from './SideBar.style';
+import React from 'react';
+
+const SideBar = () => {
+	return (
+		<S.SideBarWrapper>
+			<SideBar></SideBar>
+		</S.SideBarWrapper>
+	);
+};
+
+export default SideBar;
---
+++ src/components/SideBar/SideBar.style.js
@@ -0,0 +1,14 @@
+import styled from 'styled-components';
+
+export const SideBarWrapper = styled.section`
+	position: absolute;
+	/* bottom: 0; */
+
+	position: fixed;
+`;
+export const SideBar = styled.div`
+	width: 39rem;
+	height: 8.4rem;
+	background-color: Red;
+	position: absolute;
+`;
