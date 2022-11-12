3591f0512c1fe3610089bcac19e34e6b233a1217
gueit214 <gueit214@naver.com>
2022-11-12 15:34:34 +0900

    fix : Login페이지 리팩토링

--- src/components/Login/LoginAuth/LoginAuth.jsx
+++ src/components/Login/LoginAuth/LoginAuth.jsx
@@ -1,50 +1,25 @@
-import axios from 'axios';
-import { useState, useEffect } from 'react';
+import { useSearchParams } from 'react-router-dom';
 
 const LoginAuth = () => {
-	// const onLogin = (email, password) => {
-	// 	const data = { email, password };
-	// 	axios
-	// 		.post('/api/auth/silent-refresh', data)
-	// 		.then(response => {
-	// 			const { accessToken } = response.data;
-	// 			console.log('액세스 토큰', accessToken);
-	// 			axios.defaults.headers.common.Authorization = `Bearer ${accessToken}`;
-	// 		})
-	// 		.catch(error => {
-	// 			// 에러 처리
-	// 		});
-	// };
+	const [searchParams, setSearchParams] = useSearchParams();
+	const email = searchParams.getAll('email')[0];
+	const provider = searchParams.getAll('provider')[0];
+	const role = searchParams.getAll('role')[0];
+	const sort = searchParams.getAll('sort')[0];
+	const providerId = searchParams.getAll('providerId')[0];
 
-	// useEffect(() => {
-	// 	onLogin();
-	// }, []);
-
-	const basicInfo = JSON.parse(localStorage?.getItem('basicInfo'));
-	const role = new URL(window.location.href).searchParams.get('role');
-	const sort = new URL(window.location.href).searchParams.get('sort');
-	const email = new URL(window.location.href).searchParams.get('email');
-	const providerId = new URL(window.location.href).searchParams.get('providerId');
-	const provider = new URL(window.location.href).searchParams.get('provider');
-
-	useEffect(() => {
-		localStorage.setItem(
-			'basicInfo',
-			JSON.stringify({ ...basicInfo, email, provider, providerId }),
-		);
-	}, []);
-
+	console.log(1);
 	if (role === 'guest') {
 		if (sort === 'true') {
 			// 아직 심사 대기 상태인 경우 -> 대기 화면
-			window.location.href = `http://localhost:3000//wait`;
+			window.location.href = `http://localhost:3000/wait`;
 		} else {
 			// 가입이 필요한 경우
 			window.location.href = `http://localhost:3000/basicInfo`;
 		}
 	} else {
 		// 모든 가입 완료한 상태 -> 피드 화면
-		// window.location.href = ``;
+		window.location.href = `http://localhost:3000/home`;
 	}
 };
 
--- src/components/Login/LoginBtn/LoginBtn.jsx
+++ src/components/Login/LoginBtn/LoginBtn.jsx
@@ -5,11 +5,10 @@ import kakaoLogin from '../../../assets/kakaoLogin.png';
 import googleLogin from '../../../assets/googleLogin.png';
 import naverLogin from '../../../assets/naverLogin.png';
 
-const HOME = `http://ec2-3-34-75-204.ap-northeast-2.compute.amazonaws.com:8080`;
 const REDIRECT_URI = 'http://localhost:3000/oauth2/redirect';
-const Naver = `${HOME}/api/oauth2/naver?redirect_uri=${REDIRECT_URI}`;
-const Google = `${HOME}/api/oauth2/google?redirect_uri=${REDIRECT_URI}`;
-const Kakao = `${HOME}/api/oauth2/kakao?redirect_uri=${REDIRECT_URI}`;
+const Naver = `${process.env.REACT_APP_API_SERVER_HOST}/api/oauth2/naver?redirect_uri=${REDIRECT_URI}`;
+const Google = `${process.env.REACT_APP_API_SERVER_HOST}/api/oauth2/google?redirect_uri=${REDIRECT_URI}`;
+const Kakao = `${process.env.REACT_APP_API_SERVER_HOST}/api/oauth2/kakao?redirect_uri=${REDIRECT_URI}`;
 
 const LoginBtn = () => {
 	return (
@@ -17,15 +16,15 @@ const LoginBtn = () => {
 			<S.Logo />
 			<S.Title>가면 속 운명의 상대를 만나보세요, 마스크팅!</S.Title>
 			<S.LoginWrapper>
-				<a href={Kakao}>
+				<S.LoginButton href={Kakao}>
 					<S.LoginImg alt="카카오톡 로그인" src={kakaoLogin} />
-				</a>
-				<a href={Naver}>
+				</S.LoginButton>
+				<S.LoginButton href={Naver}>
 					<S.LoginImg alt="네이버 로그인" src={naverLogin} />
-				</a>
-				<a href={Google}>
+				</S.LoginButton>
+				<S.LoginButton href={Google}>
 					<S.LoginImg alt="구글 로그인" src={googleLogin} />
-				</a>
+				</S.LoginButton>
 			</S.LoginWrapper>
 		</Wrapper>
 	);
