fbd7eae02e3cd74c68ccb15337e8ba036faa66ab
jsl0149 <jsl0149@naver.com>
2023-01-16 22:35:26 +0900

    feat : 좋아요 한 사람들의 리스트 출력 기능 좋아요를 누른 사람의 리스트를 출력하는 기능을 구현하기 위해 컴포넌트를 추가했습니다.

--- src/pages/Chatting/Request/ChattingRequestPage.js
+++ src/pages/Chatting/Request/ChattingRequestPage.js
@@ -1,24 +1,108 @@
 import * as S from './ChattingRequestPage.style';
-import React from 'react';
+import { useState } from 'react';
 import Wrapper from '../../../components/Wrapper';
 import { WrapperInner } from '../../../components/Wrapper/Wrapper';
+import { getLikeList } from '../../../api/getLikeList';
 import SideBar from '../../../components/SideBar/SideBar';
-
+import Panel from '../../../components/Panel/Panel';
+import PictureCircle from '../../../components/PictureCircle/PictureCircle';
+import ChattingFeedPage from '../RequestFeed/ChattingFeedPage';
+import { useEffect } from 'react';
 const ChattingRequestPage = () => {
-	return (
-		<Wrapper titleMessage="채팅">
-			<WrapperInner>
-				<S.NotifyBox>
-					<S.NotifyTextBox>
-						<S.NotifyInfoIfNoRoom>
-							분당청소요정님의 피드를 추천받고 대화를 나누고 싶어하시는 분들이에요
-						</S.NotifyInfoIfNoRoom>
-					</S.NotifyTextBox>
-				</S.NotifyBox>
-			</WrapperInner>
-			<SideBar status="chatting" />
-		</Wrapper>
-	);
+	const [feedList, setFeedList] = useState([
+		{
+			nickname: '테스트닉네임',
+			profile:
+				'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQCu6isdSdhj42s-lS8LNe98m4ilRLgYPiXOQ&usqp=CAU',
+			bio: '안녕하세요',
+			feed: [],
+		},
+	]);
+
+	const [feedViewState, setFeedViewState] = useState(false);
+	const [selectedFeed, setSelectedFeed] = useState(0);
+
+	const init = async () => {
+		const feedList = await getLikeList();
+		console.log(feedList);
+	};
+
+	useEffect(() => {
+		init();
+	}, []);
+
+	const handleFeedButton = e => {
+		setSelectedFeed(parseInt(e.currentTarget.id));
+		setFeedViewState(true);
+	};
+
+	const FEEDPHOTOLIST = [
+		{
+			id: '1',
+			src: 'http://news.samsungdisplay.com/wp-content/uploads/2018/08/8.jpg',
+		},
+		{
+			id: '2',
+			src: 'http://news.samsungdisplay.com/wp-content/uploads/2018/08/8.jpg',
+		},
+		{
+			id: '3',
+			src: 'http://news.samsungdisplay.com/wp-content/uploads/2018/08/8.jpg',
+		},
+		{
+			id: '4',
+			src: 'http://news.samsungdisplay.com/wp-content/uploads/2018/08/8.jpg',
+		},
+		{
+			id: '5',
+			src: 'http://news.samsungdisplay.com/wp-content/uploads/2018/08/8.jpg',
+		},
+		{
+			id: '6',
+			src: 'http://news.samsungdisplay.com/wp-content/uploads/2018/08/8.jpg',
+		},
+	];
+
+	if (feedViewState)
+		return (
+			<ChattingFeedPage
+				setViewState={setFeedViewState}
+				userInfo={feedList[selectedFeed]}
+			></ChattingFeedPage>
+		);
+	else
+		return (
+			<Wrapper titleMessage="채팅">
+				<WrapperInner>
+					<S.NotifyBox>
+						<S.NotifyTextBox>
+							<S.NotifyInfoIfNoRoom>
+								분당청소요정님의 피드를 추천받고 대화를 나누고 싶어하시는 분들이에요
+							</S.NotifyInfoIfNoRoom>
+						</S.NotifyTextBox>
+					</S.NotifyBox>
+					{feedList.map((feedItem, idx) => (
+						<Panel size="midium" key={idx}>
+							<S.PanelFeedInner onClick={handleFeedButton} id={idx}>
+								<S.FeedProfile>
+									<PictureCircle src={feedItem.profile} size="small" />
+									<S.FeedProfileInfo>{feedItem.nickname}</S.FeedProfileInfo>
+								</S.FeedProfile>
+								<S.FeedInfo>
+									<S.InfoMidium>{feedItem.bio}</S.InfoMidium>
+								</S.FeedInfo>
+								<S.FeedImageList>
+									{FEEDPHOTOLIST.map(FeedItem => (
+										<S.FeedImageItem key={FeedItem.id} src={FeedItem.src} />
+									))}
+								</S.FeedImageList>
+							</S.PanelFeedInner>
+						</Panel>
+					))}
+				</WrapperInner>
+				<SideBar status="chatting" />
+			</Wrapper>
+		);
 };
 
 export default ChattingRequestPage;
--- src/pages/Chatting/Request/ChattingRequestPage.style.js
+++ src/pages/Chatting/Request/ChattingRequestPage.style.js
@@ -11,6 +11,7 @@ const NotifyBox = styled.section`
 	background-color: ${PALETTE.BLACK200};
 	display: flex;
 	align-items: center;
+	margin-bottom: 15px;
 `;
 
 const NotifyTextBox = styled.div`
@@ -30,6 +31,70 @@ const NotifyInfoIfNoRoom = styled.p`
 	text-align: center;
 	padding: 6rem;
 	font-weight: 700;
+`;
+
+export const ModalInner = styled.div`
+	padding: 1rem 0;
+	display: flex;
+	flex-direction: column;
+	align-items: center;
+	justify-content: space-evenly;
+`;
+
+export const PanelInfoInner = styled.div`
+	width: 100%;
+	height: 100%;
+	display: flex;
+	flex-direction: column;
+	align-items: center;
+	justify-content: space-evenly;
+`;
+
+export const PanelFeedInner = styled.div`
+	width: 100%;
+	height: 100%;
+	position: relative;
+	cursor: pointer;
+`;
+
+export const InfoBig = styled.p`
+	font-size: ${FONTSIZE.subTitle};
+	font-weight: 700;
+`;
+
+export const InfoMidium = styled.p`
+	font-size: ${FONTSIZE.text};
+`;
+
+export const InfoSmall = styled.p`
+	color: ${PALETTE.BLACK600};
+	margin: 1rem 0;
+`;
+
+export const FeedImageList = styled.section`
+	bottom: 0;
+	position: absolute;
+`;
+
+export const FeedImageItem = styled.img`
+	width: 11.4rem;
+	height: 11.4rem;
+`;
+
+export const FeedProfile = styled.article`
+	display: flex;
+	align-items: center;
+	padding: 2rem 3rem;
+`;
+export const FeedProfileInfo = styled.p`
+	font-size: ${FONTSIZE.subTitle};
+	font-weight: 700;
+
+	margin-left: 2rem;
+`;
+
+export const FeedInfo = styled.article`
+	padding: 0 2rem;
 `;
 
 export { NotifyBox, NotifyTextBox, NotifyInfoIfNoRoom };
