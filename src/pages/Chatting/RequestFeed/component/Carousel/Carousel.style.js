6206d8575dad4c9c1da8e525ab6bfa466539422b
jsl0149 <jsl0149@naver.com>
2023-02-02 23:10:41 +0900

    feat : 피드 캐러셀 기능 추가

---
+++ src/pages/Chatting/RequestFeed/component/Carousel/Carousel.style.js
@@ -0,0 +1,52 @@
+import styled, { keyframes } from 'styled-components';
+
+const bigger = keyframes`
+	0% {
+		transform: scale(0);
+	}
+	100% {
+		transform: scale(1);
+	}
+`;
+
+export const CarouselWrapper = styled.div`
+	position: absolute;
+	width: 342px;
+	height: 342px;
+	animation: ${bigger} 0.2s;
+`;
+
+export const CarouselItem = styled.div`
+	position: relative;
+	float: left;
+	flex-grow: 2;
+	width: 342px;
+	height: 342px;
+
+	background: url(${props => props.src});
+	background-size: cover;
+	/* Black/50 */
+	overflow: hidden;
+	border: 1px solid #fafafa;
+	border-radius: 16px;
+`;
+
+export const NavigatePrev = styled.img`
+	position: absolute;
+	left: -30px;
+	top: 122px;
+	z-index: 2;
+	width: 100px;
+	height: 100px;
+	cursor: pointer;
+`;
+
+export const NavigateNext = styled.img`
+	position: absolute;
+	top: 122px;
+	right: -30px;
+	z-index: 2;
+	width: 100px;
+	height: 100px;
+	cursor: pointer;
+`;
