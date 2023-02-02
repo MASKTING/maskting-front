b912fc6d44c7e38d040abeb2b4c7f871bffde708
jsl0149 <jsl0149@naver.com>
2023-02-03 01:48:04 +0900

    feat : 대화 승낙, 거절 모달 구현

---
+++ src/pages/Chatting/RequestFeed/component/Modals/DeclineModal/DeclineModal.jsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import Modal from '../../../../../../components/Modal';
+import SmallButton from '../../../../../../components/Button/SmallButton/SmallButton';
+import * as S from './DeclineModal.style';
+
+const DeclineModal = ({ changeModalState, userInfo }) => {
+	const handleDeclineConfirm = async e => {
+		changeModalState(e);
+	};
+
+	return (
+		<Modal width="32.1" height="23.2" onCloseModal={changeModalState}>
+			<S.ModalInner>
+				<S.Title>
+					{userInfo?.nickname}님과의
+					<br />
+					대화를 정말 거절하시겠어요?
+				</S.Title>
+				<S.Info>
+					상대방의 대화 요청을 거절할 경우 <br></br>더 이상 추천 파트너로 업데이트 되지 않아요!
+				</S.Info>
+				<S.SmallButton data-state={'declineNotice'} onClick={handleDeclineConfirm}>
+					거절하기
+				</S.SmallButton>
+				<SmallButton color="white" onClick={changeModalState}>
+					취소
+				</SmallButton>
+			</S.ModalInner>
+		</Modal>
+	);
+};
+
+export default DeclineModal;
