d696eb8b95be0feb657a67a852e22cf624eec67f
Kuesung Park <gueit214@naver.com>
2023-02-23 21:11:57 +0900

    refactor : 폴더 위치 변경 및 style 분리

---
+++ src/pages/Wait/Wait/Wait.style.js
@@ -0,0 +1,15 @@
+import styled from 'styled-components';
+export const Title = styled.section`
+	font-size: 2.6rem;
+	color: #212121;
+	font-weight: 700;
+	line-height: 1.4;
+`;
+export const Content = styled.div`
+	font-size: 1.3rem;
+	line-height: 1.3;
+`;
+export const InfoTitle = styled.ul`
+	margin-top: 3rem;
+`;
+export const Line = styled.li``;
