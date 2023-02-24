3cc10f11a1f77ae8c49ae3de32b4b5fbbc060edd
Kuesung Park <gueit214@naver.com>
2023-02-24 15:53:09 +0900

    refactor : 티켓 아이콘 변경

--- src/components/Home/MainHeader/MainHeader.js
+++ src/components/Home/MainHeader/MainHeader.js
@@ -1,21 +1,35 @@
 import * as S from './MainHeader.style';
 
-import React from 'react';
+import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
+import WaitingModal from '../../common/WaitingModal';
+
+import ticketIcon from '../../../assets/svg/icon_ticket.svg';
 
 const MainHeader = () => {
 	const navigate = useNavigate();
-	const navigateNotification = () => {
-		navigate('notification');
-	};
+	const [waitingModal, setWaitingModal] = useState(false);
 	return (
 		<S.MainHeader>
+			{waitingModal && <WaitingModal isModal={WaitingModal} />}
 			<S.TicketSection>
-				<S.TicketIcon className="material-icons">local_activity</S.TicketIcon>
+				<img
+					src={ticketIcon}
+					alt="티켓"
+					onClick={() => {
+						navigate('/ticket');
+					}}
+				/>
 				<S.TicketInfo>30</S.TicketInfo>
 			</S.TicketSection>
 			<S.NotificationSection>
-				<S.NotificationIcon className="material-icons" onClick={navigateNotification}>
+				<S.NotificationIcon
+					className="material-icons"
+					onClick={() => {
+						// navigate('notification');
+						setWaitingModal(true);
+					}}
+				>
 					notifications
 				</S.NotificationIcon>
 			</S.NotificationSection>
