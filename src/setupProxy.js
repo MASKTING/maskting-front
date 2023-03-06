22fad6b3e1bc06bd63ac583c755abd97979f61b9
jsl0149 <jsl0149@naver.com>
2023-03-06 19:09:40 +0900

    fix : 소켓 프록시 설정 삭제

--- src/setupProxy.js
+++
@@ -1,5 +0,0 @@
-const { createProxyMiddleware } = require('http-proxy-middleware');
-
-module.exports = app => {
-	app.use(createProxyMiddleware('/app', { target: 'http://localhost:8080/', ws: true }));
-};
