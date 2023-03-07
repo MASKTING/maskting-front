1ddccbe2323948dd125f2b844b6b17725eca8c5b
jsl0149 <jsl0149@naver.com>
2023-03-08 00:04:29 +0900

    fix : 인증번호 수정

--- src/api/postReceiveVerifiactionNumber.js
+++ src/api/postReceiveVerifiactionNumber.js
@@ -5,11 +5,11 @@ export const postReceiveVerificationNumber = async phoneNumber => {
 	try {
 		const formData = new FormData();
 		formData.append('phoneNumber', phoneNumber);
-		const response = await api({
+		const response = await axios({
 			headers: {
 				'Content-Type': 'multipart/form-data',
 			},
-			url: '/api/user/sms',
+			url: `${process.env.REACT_APP_SERVER_HOST}/api/user/sms`,
 			method: 'POST',
 			data: formData,
 		});
