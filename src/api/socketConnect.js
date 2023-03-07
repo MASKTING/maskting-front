ebb0b33ed69482385e0d712d671d0a206077ebcc
jsl0149 <jsl0149@naver.com>
2023-03-07 22:51:06 +0900

    fix : 443 port 붙여보기

--- src/api/socketConnect.js
+++ src/api/socketConnect.js
@@ -7,11 +7,11 @@ import * as StompJs from '@stomp/stompjs';
  */
 const createClient = endpoint => {
 	const client = new StompJs.Client({
-		brokerURL: `wss://maskting.site${endpoint}`,
+		brokerURL: `wss://maskting.site:443${endpoint}`,
 	});
 
 	client.webSocketFactory = () => {
-		const socketIn = new SockJS(`https://maskting.site${endpoint}`);
+		const socketIn = new SockJS(`https://maskting.site:443${endpoint}`);
 		return socketIn;
 	};
 
